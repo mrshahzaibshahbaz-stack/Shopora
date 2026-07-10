888876b708ba7e74132653338323e39cce070a53
Shahzaib554 <mrshahzaibshahbaz@gmail.com>
2026-07-11 01:03:12 +0500

    Meditaton Component Added

---
+++ src/components/Meditation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Meditation = () => {
+  return (
+    <View>
+      <Text>Meditation</Text>
+    </View>
+  )
+}
+
+export default Meditation
+
+const styles = StyleSheet.create({})
