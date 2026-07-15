7de22c670561398e06fab6e6c6a23ba60f503b96
Shahzaib554 <mrshahzaibshahbaz@gmail.com>
2026-07-15 23:20:04 +0500

    Payment Screen Complete with dynamic button's

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,83 @@
+// components/PrimaryButton.tsx
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
+
+interface PrimaryButtonProperty {
+  title: string;
+  onPress?: () => void;
+  disabled?: boolean;
+  icon?: string;
+  iconSize?: number;
+  textColor?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  borderWidth?: number;
+}
+
+export const PrimaryButton = ({
+  title,
+  onPress,
+  disabled = false,
+  icon,
+  iconSize = 15,
+  textColor = '#FF7622',
+  backgroundColor = '#FFFFFF',
+  borderColor = '#F0F5FA',
+  borderWidth = 2,
+}: PrimaryButtonProperty) => {
+  
+
+  const finalTextColor = disabled ? '#B0B0B0' : textColor;
+  const finalBgColor = disabled ? '#F0F0F0' : backgroundColor;
+  const finalBorderColor = disabled ? '#E0E0E0' : borderColor;
+
+  return (
+    <TouchableOpacity
+      style={[
+        styles.button,
+        {
+          backgroundColor: finalBgColor,
+          borderColor: finalBorderColor,
+          borderWidth: borderWidth,
+          opacity: disabled ? 0.6 : 1,
+        },
+      ]}
+      onPress={onPress}
+      disabled={disabled}
+      activeOpacity={0.8}
+    >
+      {icon && (
+        <Icon
+          name={icon}
+          size={iconSize}
+          color={finalTextColor}
+          style={styles.iconSpacing}
+        />
+      )}
+
+      <Text style={[styles.text, { color: finalTextColor }]}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 18,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  iconSpacing: {
+    marginRight: 5,
+  },
+});
+
+export default PrimaryButton;
--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import BackButton from '../components/BackButton';
-import { s } from 'react-native-size-matters';
+import { s, vs } from 'react-native-size-matters';
 import { PaymentList } from '../components/PaymentList';
 import { CardView } from '../components/CardView';
+import PrimaryButton from '../components/PrimaryButton';
 
 export const PaymentScreen = () => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <View style={styles.container}>
         <BackButton />
         <Text style={styles.paymentText}>Payment</Text>
@@ -15,8 +16,36 @@ export const PaymentScreen = () => {
       <View>
         <PaymentList />
       </View>
-      <View style={{marginHorizontal: s(20), justifyContent: "center", alignItems: "center" }}>
+      <View
+        style={{
+          marginHorizontal: s(20),
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <CardView />
+      </View>
+      <View style={{ marginHorizontal: s(20) }}>
+        <PrimaryButton title="Add New" icon="plus" />
+      </View>
+      <View style={styles.priceContainer}>
+        <Text style={styles.totalText}>TOTAL:</Text>
+        <Text style={styles.priceText}>$96</Text>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 20, 
+          left: 20, 
+          right: 20, 
+        }}
+      >
+        <PrimaryButton
+          title="PAY & CONFIRM"
+          borderWidth={0}
+          backgroundColor="#FF7622"
+          textColor="#ffffff"
+        />
       </View>
     </View>
   );
@@ -34,5 +63,20 @@ const styles = StyleSheet.create({
     color: '#181C2E',
     marginStart: s(15),
     fontWeight: '600',
+  },
+  priceContainer: {
+    marginHorizontal: s(20),
+    marginTop: vs(70),
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  totalText: {
+    color: '#A0A5BA',
+    fontWeight: '400',
+    fontSize: 14,
+    marginRight: s(10),
+  },
+  priceText: {
+    fontSize: 30,
   },
 });
