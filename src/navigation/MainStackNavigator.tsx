bdfcf6885fbdab03119c92aeb662601114da16b5
Shahzaib554 <mrshahzaibshahbaz@gmail.com>
2026-07-22 01:45:05 +0500

    Navigation Changes and fixies

--- src/navigation/MainStackNavigator.tsx
+++ src/navigation/MainStackNavigator.tsx
@@ -1,23 +1,26 @@
-import React from "react";
-import { StatusBar } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { LoginScreen } from "../screens/LoginScreen";
-import HomeScreen from "../screens/HomeScreen";
-import CategoriesScreen from "../screens/CategoriesScreen";
-import OnboardingScreen from "../screens/OnboardingScreen";
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import CategoriesScreen from '../screens/CategoriesScreen';
+import OnboardingScreen from '../screens/OnboardingScreen';
+import { LoginScreen } from '../screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export function MainStackNavigator() {
-    return (
-        <>
-            <StatusBar barStyle="dark-content" backgroundColor="white" />
-            <Stack.Navigator initialRouteName="OnboardingScreen">
-                <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </>
-    )
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+      <Stack.Navigator
+        initialRouteName="OnboardingScreen"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} />
+      </Stack.Navigator>
+    </>
+  );
 }
